a860f57b28a6dcc775107e3a43d3ac61b4c5a630
Harshit Jain <reach@harsh1998.dev>
2025-01-10 00:41:10 +0530

    nabu: add android-removed-permissions mark telecom disabled
    Check: https://www.diffchecker.com/UHR1VVVl/
    
    Signed-off-by: Harshit Jain <reach@harsh1998.dev>

--- device.mk
+++ device.mk
@@ -80,9 +80,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
     frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
-    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
+    $(LOCAL_PATH)/configs/android-removed-permissions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android-removed-permissions.xml
 
-# Device is a Tablet
+# Device is a Tableandroid-removed-permissions.xmlt
 PRODUCT_CHARACTERISTICS := tablet
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1600
